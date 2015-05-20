9ba658d43c0520c8ad1aad71e1273a7a81c10ffe
Chris Jarvis <clj@ollure.com>
2015-05-20 10:55:55 -0500

    edit console logging

--- public/javascripts/shopHandler.js
+++ public/javascripts/shopHandler.js
@@ -2,11 +2,13 @@ $(document).ready(function() {
 
   // User clicked on an edit button
   $(".editButton").click(function () {
-    window.location.href = "/shop" + $(this)[0].id;
+    console.log("you clicked the edit button!")
+    window.location.href = "/shop/" + $(this)[0].id;
   });
 
   // User clicked on a delete button
   $(".deleteButton").click(function () {
+    console.log("you clicked the delete button!")
     var shopItemId = $(this)[0].id;
 
     $.ajax({
